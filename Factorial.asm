9ffc69c097dcad6d5e37c2e089644d0efb1f62fa
Cayden Ronning <159830204+Cayden-Ronning@users.noreply.github.com>
2024-05-07 22:36:33 -0400

    Fixed indentation

--- Factorial.asm
+++ Factorial.asm
@@ -3,21 +3,21 @@
 # (a factorial is the product of all positive integers 
 # less than or equal to a given positive integer)
 
-			.text
+		.text
 # Ask for a number
 restart:
 			
-			la		   $a0, question # Load address of question string
-			li		   $v0, 4		 # syscall 4 prints a string
-			syscall
+		la	   $a0, question 	 # Load address of question string
+		li	   $v0, 4	 	 # syscall 4 prints a string
+		syscall
 			
-    		addi       $a0, $0, 10   # ASCII code 10 is a line feed
-    		li         $v0, 11       # syscall 11 prints character in $a0
+    		addi       $a0, $0, 10   	 # ASCII code 10 is a line feed
+    		li         $v0, 11       	 # syscall 11 prints character in $a0
     		syscall
 
 # Read in the number
 
-    		li         $v0, 5        # syscall 5 reads in an integer
+    		li         $v0, 5        	 # syscall 5 reads in an integer
     		syscall
     		
     		move	   $t0, $v0		 # Store result in $t0 (prints at the end)
@@ -26,59 +26,59 @@ restart:
 
 # Check for invalid input
 
-			blez 	   $t0, invalid	 # If input is less than or equal to 0...
-			b		   valid		 # Otherwise, continue as normal
+		blez 	   $t0, invalid	 	 # If input is less than or equal to 0...
+		b	   valid	 	 # Otherwise, continue as normal
 			
 invalid:
 
-			# Print error message
-			la		   $a0, pInvalid # Load address of pInvalid string
-			li		   $v0, 4		 # syscall 4 prints a string
-			syscall
+		# Print error message
+		la	   $a0, pInvalid 	 # Load address of pInvalid string
+		li	   $v0, 4	 	 # syscall 4 prints a string
+		syscall
 			
-    		addi       $a0, $0, 10   # ASCII code 10 is a line feed
-    		li         $v0, 11       # syscall 11 prints character in $a0
+    		addi       $a0, $0, 10   	 # ASCII code 10 is a line feed
+    		li         $v0, 11       	 # syscall 11 prints character in $a0
     		syscall
     		syscall
 
-    		b		   restart		 # Ask for a number again
+    		b	   restart		 # Ask for a number again
     		
 valid:
 
 # Calculate factorial value
 multLoop:
 			
-			# Increment down by one; if 0 reached, end loop
-			add		   $t1, $t1, -1
-			beq 	   $t1, 0, exitLoop
+		# Increment down by one; if 0 reached, end loop
+		add	   $t1, $t1, -1
+		beq 	   $t1, 0, exitLoop
 			
-			mult	   $t2, $t1		 # Multiply current total by next number
-			mflo	   $t2
+		mult	   $t2, $t1		 # Multiply current total by next number
+		mflo	   $t2
 
-			b		   multLoop
+		b	   multLoop
 			
 exitLoop:
 
-			# Print starting number
-			move	   $a0, $t0
-			li		   $v0, 1		 # syscall 1 prints an integer
-			syscall
+		# Print starting number
+		move	   $a0, $t0
+		li	   $v0, 1		 # syscall 1 prints an integer
+		syscall
 
-			# Print "! = "
-			la		   $a0, print	 # Load address of print string
-			li		   $v0, 4		 # syscall 4 prints a string
-			syscall
+		# Print "! = "
+		la	   $a0, print	 	 # Load address of print string
+		li	   $v0, 4	 	 # syscall 4 prints a string
+		syscall
 			
-			# Print result
-			move	   $a0, $t2
-			li		   $v0, 1		 # syscall 1 prints an integer
-			syscall
+		# Print result
+		move	   $a0, $t2
+		li	   $v0, 1		 # syscall 1 prints an integer
+		syscall
 			
-    		li         $v0, 10        # syscall 10 terminates program execution
+    		li         $v0, 10        	 # syscall 10 terminates program execution
     		syscall
 		
 # Stores text for printing
-			.data
+		.data
 question:	.asciiz "Please provide a positive integer to find the factorial:"
 print:		.asciiz "! = "
 pInvalid:	.asciiz "Not a positive integer!"
